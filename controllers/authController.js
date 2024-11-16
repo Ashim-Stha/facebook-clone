fc99571caee1d68d95f7ff7ae92d38d5cb7b9d54
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-11-16 20:34:42 +0545

    generateToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,24 @@
 const User = require("../model/User");
+const response = require("../utils/responseHandler");
+const bcrypt = require("bcryptjs");
 const registerUser = async (req, res) => {
   try {
     const { username, email, password, gender, dateOfBirth } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
+      return response(res, 400, "User with this email already exists");
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      gender,
+      dateOfBirth,
+    });
+
+    await newUser.save();
   } catch (e) {}
 };
