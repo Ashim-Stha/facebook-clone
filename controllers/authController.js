df35c506bef219562865e1068d1ead39d29264fd
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-11-17 10:00:50 +0545

    registerUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../model/User");
 const response = require("../utils/responseHandler");
 const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils/generateToken");
 const registerUser = async (req, res) => {
   try {
     const { username, email, password, gender, dateOfBirth } = req.body;
@@ -20,5 +21,20 @@ const registerUser = async (req, res) => {
     });
 
     await newUser.save();
-  } catch (e) {}
+
+    const accessToken = generateToken(newUser);
+    res.cookie("auth_token", accessToken, {
+      httpOnly: true,
+    });
+
+    return response(res, 201, "User created successfully", {
+      username: newUser.username,
+      email: newUser.email,
+    });
+  } catch (e) {
+    console.error(e);
+    return response(res, 500, "Internal Server Error", e.message);
+  }
 };
+
+module.exports = { registerUser };
