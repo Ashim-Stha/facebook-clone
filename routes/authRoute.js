6ccfa19790c0779443274d832b15e5ab02b26575
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-11-18 18:03:11 +0545

    passport-google-oauth2 google signin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,10 +4,35 @@ const {
   loginUser,
   logout,
 } = require("../controllers/authController");
+const passport = require("../controllers/googleController");
+const { generateToken } = require("../utils/generateToken");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
 
+//google oauth route
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+//google callback route
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}/user-login`,
+    session: false,
+  }),
+  (req, res) => {
+    const accessToken = generateToken(req?.user);
+    res.cookie("auth_token", accessToken, {
+      httpOnly: true,
+    });
+    res.redirect(`${process.env.FRONTEND_URL}`);
+  }
+);
 module.exports = router;
